0d1de3b6a24e3b55d44f851600b431d75b78ce42
Spark631 <69885780+Spark631@users.noreply.github.com>
2022-04-17 02:20:59 -0400

    Main

--- prediction.py
+++ prediction.py
@@ -25,20 +25,12 @@ merged_data = pd.concat([true_data,fake_data],axis=0)
 md = merged_data.head()
 
 #this will remove every collumn besides the texy and the 0, 1 value
-unnecessary = merged_data.drop(["title","subject","date"],axis=1)
+unnecessary = merged_data.drop(["subject","title","date"],axis=1)
 uh = unnecessary.head()
 
 #this is mixing the data
 unnecessary = unnecessary.sample(frac=1)
 uh = unnecessary.head()
-
-# def clean_data(data):
-#   data = re.sub('https?://\S+|www.\S+','',data)
-
-# unnecessary["text"] = unnecessary["text"].apply(clean_data)
-# uh = unnecessary.head()
-
-# print(uh)
 
 x = unnecessary["text"]
 y = unnecessary["class"]
@@ -51,7 +43,6 @@ from sklearn.linear_model import LogisticRegression
 vector = TfidfVectorizer()
 xv_train  = vector.fit_transform(x_train)
 xv_test = vector.transform(x_test)
-# print(f"THIS IS THE THE XV_TEST SHIZ: {xv_test}")
 LR = LogisticRegression()
 LR.fit(xv_train,y_train)
 
@@ -61,32 +52,21 @@ pred_LR = LR.predict(xv_test)
 print(classification_report(y_test,pred_LR))
 
 def output_label(n):
-    print(f"THis is n: {n}" )
     if n ==0:
-        return "Fake News"
+        return False
+        # return "The text is Fake News"
     elif n==1:
-        return "True News"
-    else:
-        return "ERROR!!"
+        return True
+        # return "The text is True News"
 def manual_testing(news):
     testing_news = {"text":[news]}
-    print(f"THIS IS THE NEWS: {news}")
     new_def_test = pd.DataFrame(testing_news)
     new_x_test = new_def_test['text']
     new_xv_test = vector.transform(new_x_test)
     pred_LR = LR.predict(new_xv_test)
-    print(f"THIS IS THE PRED_LR{pred_LR}")
-    # pred_DT = DT.predict(new_xv_test)
-    # pred_GBC = GBC.predict(new_xv_test)
-    # pred_RFC = RFC.predict(new_xv_test)
     
-    return print(f'''\n
-                    LR Prediction: {output_label(pred_LR)}\n
-                 ''')
-                    # DT Prediction: {output_label(pred_DT)}\n
-                    # GBC Prediction: {output_label(pred_GBC)}\n
-                    # RFC Prediction: {output_label(pred_RFC)}\n
+    theData = output_label(pred_LR)
+    return theData
+    # return print(f'LR Prediction: {output_label(pred_LR)}\n')
 
-news = input("News Here")
-manual_testing(news)
 
--- views.py
+++ views.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
-
+from re import X
+from unicodedata import category
+from flask import Blueprint, render_template, request, flash
+from prediction import manual_testing
 views = Blueprint("views", __name__)
 
-@views.route("/")
+@views.route("/", methods=["GET", "POST"])
 def home():
-  return render_template("static/home.html")    
+    if request.method == "POST":
+        data = request.form.get("add")
+        print(data)
+        output = (manual_testing(data))
+
+        if output == True:
+            flash("This source is secure and should be safe to use ", category="success")
+        elif output == False:
+            flash("This source is fake and unreliable. Please use this source with caution.", category="error")
+        
+    return render_template("home.html")
